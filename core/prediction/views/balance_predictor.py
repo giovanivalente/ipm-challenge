ae75fb361af1a00bd87aa7c3aa381081061a0470
valnog <nogueiravalente@gmail.com>
2025-05-23 09:52:01 -0300

    doc: add swagger to balance predictor

--- core/prediction/views/balance_predictor.py
+++ core/prediction/views/balance_predictor.py
@@ -1,9 +1,11 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.prediction.factory import PredictionFactory
 from core.prediction.serializers import BalanceInputSerializer, BalanceOutputSerializer
+from core.shared.schema_exceptions import STANDARD_ERROR_RESPONSES
 
 
 class BalancePredictionAPIView(APIView):
@@ -13,6 +15,13 @@ class BalancePredictionAPIView(APIView):
         super().__init__(**kwargs)
         self.load_balance_predictor = PredictionFactory.make_load_balance_predictor()
 
+    @extend_schema(
+        tags=['Prediction'],
+        request=BalanceInputSerializer,
+        responses={200: BalanceOutputSerializer, **STANDARD_ERROR_RESPONSES},
+        summary='Prediction Balance',
+        description='Predict future balance.',
+    )
     def post(self, request) -> Response:
         serializer = BalanceInputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
