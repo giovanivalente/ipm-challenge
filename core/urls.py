10252af8b3e1de41e0e17acb10a6288a25de1de4
valnog <nogueiravalente@gmail.com>
2025-05-23 10:03:39 -0300

    refact: update urls

--- core/urls.py
+++ core/urls.py
@@ -19,9 +19,9 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/v1/', include('core.account.urls', namespace='account')),
     path('api/v1/', include('core.authentication.urls', namespace='authentication')),
     path('api/v1/', include('core.prediction.urls', namespace='prediction')),
